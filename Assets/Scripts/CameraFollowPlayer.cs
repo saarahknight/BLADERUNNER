3778b15a17cf7555696f799074b04e86c0d6b3a9
IudevGundyr <90079225+IudevGundyr@users.noreply.github.com>
2024-02-20 02:59:46 -0300

    katchau

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
+    public Transform jogador;
+    public Vector3 offset;
+    public float suavidadePosicao = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // offset = target.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        if (jogador != null)
+        {
+            // Calcula a posição desejada da câmera
+            Vector3 posiçãoDesejada = jogador.position + offset;
+            Vector3 posiçãoSuavizada = Vector3.Lerp(transform.position, posiçãoDesejada, suavidadePosicao);
+            transform.position = posiçãoSuavizada;
+
+            // Faz com que a câmera olhe diretamente para o jogador
+            transform.LookAt(jogador.position);
+        }
     }
 }
+
